1b1b55b8360ad1456a33d7be211be0d3c440643b
hoainam0503 <14520549@gm.uit.edu.vn>
2019-10-12 00:00:46 +0700

    Updates

---
+++ precache-manifest.f6c3d0dd8d333ddc79622ffb33724161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce113808c0975c6e51882e402f3ffe32",
+    "url": "/testpublic/index.html"
+  },
+  {
+    "revision": "cdbcab0f2ea9a89e8b10",
+    "url": "/testpublic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4f5679d1484a45e9c9d0",
+    "url": "/testpublic/static/js/2.0dafb66b.chunk.js"
+  },
+  {
+    "revision": "cdbcab0f2ea9a89e8b10",
+    "url": "/testpublic/static/js/main.087ba885.chunk.js"
+  },
+  {
+    "revision": "465bd29f9b1da213ab1b",
+    "url": "/testpublic/static/js/runtime~main.36404d74.js"
+  }
+]);
